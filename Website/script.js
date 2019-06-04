d9d1768b7e9526226501c98f79a4f165f2bef580
Nisha Choudhary <37740257+choudharynisha@users.noreply.github.com>
2019-06-04 14:24:22 -0400

    Create script.js

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+// Next step is to translate the code we have and put it in here
